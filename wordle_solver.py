75476721322e5c789135759a47e56a64d67dc85e
Dom <domlamastra@Dominicks-MacBook.local>
2022-05-09 14:21:12 -0400

    added two game modes

--- wordle_solver.py
+++ wordle_solver.py
@@ -19,14 +19,15 @@ while (valid_word == 0):
       valid_word = 1
       break
 
-
-# due to information theory, the first word to guess is always CRANE
-guess = "CRANE"
+# choose what method is being played
+mode = input("Do you want to play wordle or use calculator \n0: play wordle \n1: calculator\n")
 
 # - denotes the character is correct in the right place
 # + denotes the character is correct in the wrong place
 # x denotes the character is incorrect
 
+# define what the user's guess will be 
+guess = ""
 # string to handle current user progress
 output_string = ""
 
@@ -35,9 +36,28 @@ guess_count = 1
 
 while (wordle_word != guess):
 
+  # if the user is using a calculator
+  if (mode == 1):
+
+    # due to information theory, the first word to guess is always CRANE
+    guess = "CRANE"
+
+  # if the user is playing regular wordle
+  else:
+
+    # have the user enter a new guess
+    while (len(guess) != 5):
+
+      # have the user input their next guess
+      guess = input("Enter your next guess, must be five letters: ")
+
+      # convert guess to upper to make it easier to compare strings
+      guess = guess.upper()
+
   # show the user what there current output is
   print("Guess " + str(guess_count) + ": " + guess)
 
+  # loop through each character of the guess and the wordle word at the same time
   for i in range(0, 5):
 
     # check if the character is in the substring
@@ -49,6 +69,7 @@ while (wordle_word != guess):
         # character is in the right place so it recieves a -
         output_string += "-"
       
+      # character is in the wrong place
       else:
 
         # character is in the right place so it recieves a -
@@ -65,20 +86,14 @@ while (wordle_word != guess):
   print("OUTPUT SCORE: " + output_string)
   print("--------------------------------------------------")
 
+  # check to see if the game was finished
+  if (output_string == "-----"):
 
+    print ("Congrats, you beat the game!")
+    break 
+  
   # clear the user's old guess
   guess = ""
-
-  # have the user enter a new guess
-  while (len(guess) != 5):
-
-    # have the user input their next guess
-    guess = input("Enter your next guess, must be five letters: ")
-
-    # convert guess to upper to make it easier to compare strings
-    guess = guess.upper()
-
-
 
   # clear the output score
   output_string = ""
