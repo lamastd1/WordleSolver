f96dad2f4ec6169b818b55ddb8fba8b3d2346af6
Dom <domlamastra@Dominicks-MacBook.local>
2022-05-11 12:45:18 -0400

    set up helper methods

--- wordle_solver.py
+++ wordle_solver.py
@@ -14,138 +14,179 @@ class Word:
     if (s.possible):
       s.entropy += 1
 
-# create a list to hold all possible guesses
-guesses = []
-
-# open the list of all possible wordle words
-wordleDic = open('wordle_guesses.txt', 'r')
-
-def binary_search(line):
+# helper method algorithim to quicksort an array
+def quick_sort():
   pass
 
-# reads in all the data from the guesses file to be stored into an array
-for line in wordleDic.readlines():
-  line = line.strip()
-  line = line.upper()
-  guess = Word(line, False) 
-  guesses.append(guess)
+# helper method binary search technique to find words in the array
+def binary_search(line, guesses):
+  pass
 
-# close the guesses file
-wordleDic.close()
+# helper method to print the array
+def print_words():      
+  pass
 
-# open the list of all possible answers
-wordleAnswers = open('wordle_answers.txt', 'r')
+# append the possible words to the guesses wordlist 
+def append_wordlist(guesses):
 
-# check if a word is possible to be a valid answer
-for line in wordleAnswers.readlines():
-  line = line.strip()
-  line = line.upper()
-  binary_search(line)
+  # open the list of all possible wordle words
+  wordleDic = open('wordle_guesses.txt', 'r')
 
-# ensures that the word is a valid word
-valid_word = 0
+  # reads in all the data from the guesses file to be stored into an array
+  for line in wordleDic.readlines():
+    line = line.strip()
+    line = line.upper()
+    guess = Word(line, False) 
+    guesses.append(guess)
 
-while (valid_word == 0):
+  # close the guesses file
+  wordleDic.close()
+
+# add if each word is a possible answer
+def append_answers(guesses):
+
+  # open the list of all possible answers
+  wordleAnswers = open('wordle_answers.txt', 'r')
+
+  # check if a word is possible to be a valid answer
+  for line in wordleAnswers.readlines():
+    line = line.strip()
+    line = line.upper()
+    binary_search(line, guesses)
+
+# allow the user to enter a correct guess
+def validate_guess(guesses):
 
   # tell the user to enter a word
   wordle_word = input("Enter a five letter word: ")
   wordle_word = wordle_word.upper()
 
-  # open the list of all possible wordle words
-  wordleDic = open('wordle_answers.txt', 'r')
+  # ensures that the word is a valid word
+  valid_word = 0
 
-  # loop through each word in the set of all possible wordle words and see if the word is allowed
-  for line in wordleDic.readlines():
-    line = line.strip()
-    line = line.upper()
-    if (wordle_word == line):
-      valid_word = 1
-      break
+  while (valid_word == 0):
 
-# choose what method is being played
-mode = input("Do you want to play wordle or use calculator \n0: play wordle \n1: calculator\n")
+    # open the list of all possible wordle words
+    wordleDic = open('wordle_guesses.txt', 'r')
 
-# - denotes the character is correct in the right place
-# + denotes the character is correct in the wrong place
-# x denotes the character is incorrect
+    # loop through each word in the set of all possible wordle words and see if the word is allowed
+    for line in wordleDic.readlines():
+      line = line.strip()
+      line = line.upper()
+      if (wordle_word == line):
+        valid_word = 1
+        return wordle_word
+    
+    # tell the user to enter a word
+    validate_guess(guesses)
+  
+# plays the game 
+def play_game(wordle_word, guesses):
 
-# define what the user's guess will be 
-guess = ""
+  # choose what method is being played
+  mode = input("Do you want to play wordle or use calculator \n0: play wordle \n1: calculator\n")
 
-# string to handle current user progress
-output_string = ""
+  # - denotes the character is correct in the right place
+  # + denotes the character is correct in the wrong place
+  # x denotes the character is incorrect
 
-#calculate how many guesses you have
-guess_count = 1
+  # define what the user's guess will be 
+  guess = ""
 
-# loop until the guess is correct 
-while (wordle_word != guess):
+  # string to handle current user progress
+  output_string = ""
 
-  # if the user is using a calculator
-  if (mode == 1):
+  #calculate how many guesses you have
+  guess_count = 1
 
-    # due to information theory, the first word to guess is always CRANE
-    guess = "CRANE"
+  # loop until the guess is correct 
+  while (wordle_word != guess):
 
-  # if the user is playing regular wordle
-  else:
+    # if the user is using a calculator
+    if (mode == 1):
 
-    # have the user enter a new guess
-    while (len(guess) != 5):
+      # due to information theory, the first word to guess is always CRANE
+      guess = "CRANE"
 
-      # have the user input their next guess
-      guess = input("Enter your next guess, must be five letters: ")
+    # if the user is playing regular wordle
+    else:
 
-      # convert guess to upper to make it easier to compare strings
-      guess = guess.upper()
+      # have the user enter a new guess
+      #while (len(guess) != 5):
 
-  # show the user what there current output is
-  print("Guess " + str(guess_count) + ": " + guess)
+        # have the user input their next guess
+        #guess = input("Enter your next guess, must be five letters: ")
 
-  # loop through each character of the guess and the wordle word at the same time
-  for i in range(0, 5):
+        # convert guess to upper to make it easier to compare strings
+        #guess = guess.upper()
+      guess = validate_guess(guesses)
 
-    # check if the character is in the substring
-    if guess[i] in wordle_word:
-      
-      # check if the character is in the right position
-      if (guess[i] == wordle_word[i]):
+    # show the user what there current output is
+    print("Guess " + str(guess_count) + ": " + guess)
 
-        # character is in the right place so it recieves a -
-        output_string += "-"
+    # loop through each character of the guess and the wordle word at the same time
+    for i in range(0, 5):
+
+      # check if the character is in the substring
+      if guess[i] in wordle_word:
+        
+        # check if the character is in the right position
+        if (guess[i] == wordle_word[i]):
+
+          # character is in the right place so it recieves a -
+          output_string += "-"
+        
+        # character is in the wrong place
+        else:
+
+          # character is in the right place so it recieves a -
+          output_string += "+"
       
       # character is in the wrong place
       else:
 
-        # character is in the right place so it recieves a -
-        output_string += "+"
-    
+        # character is in the wrong place so it recieves a x
+        output_string += "x"
+
+    # give the user a menu showing their total
+    print("--------------------------------------------------")
+    print("USER GUESS:   " + guess)
+    print("OUTPUT SCORE: " + output_string)
+    print("--------------------------------------------------")
+
+    # check to see if the game was finished
+    if (output_string == "-----"):
+
+      print ("Congrats, you beat the game!")
+      break 
+
     else:
 
-      # character is in the wrong place so it recieves a x
-      output_string += "x"
+      print("mode: ")
+      print(mode)
+      if (mode == 1):
+        print("mode 1")
+        print()
 
-  # give the user a menu showing their total
-  print("--------------------------------------------------")
-  print("USER GUESS:   " + guess)
-  print("OUTPUT SCORE: " + output_string)
-  print("--------------------------------------------------")
+    # clear the user's old guess
+    guess = ""
 
-  # check to see if the game was finished
-  if (output_string == "-----"):
+    # clear the output score
+    output_string = ""
 
-    print ("Congrats, you beat the game!")
-    break 
+if __name__ == "__main__":
 
-  else:
-    if (mode == 1):
-      pass
+  # create a list to hold all possible guesses
+  guesses = []
+  
+  # append each word to the wordlist 
+  append_wordlist(guesses)
 
-  # clear the user's old guess
-  guess = ""
+  # determine if each word is an answer or not
+  append_answers(guesses)
 
-  # clear the output score
-  output_string = ""
-        
-
-
+  # recieve a valid guess from user
+  wordle_word = validate_guess(guesses)
+  
+  # play the game
+  play_game(wordle_word, guesses)
